dcacc5481d515d33d8d630765a58ef0e22eb1340
temitayo65 <iloritemitayo65@gmail.com>
2021-05-26 15:05:05 +0100

    Removes constructor in Meme Model

--- MeMe 1.0/Model.swift
+++ MeMe 1.0/Model.swift
@@ -12,11 +12,5 @@ struct Meme{
     var bottomText: String!
     var originalImage: UIImage!
     var memedImage: UIImage!
-    init(topText: String, bottomText: String, originalImage: UIImage, memedImage: UIImage){
-        self.topText = topText
-        self.bottomText = bottomText
-        self.originalImage = originalImage
-        self.memedImage = memedImage
-        }
 }
 
